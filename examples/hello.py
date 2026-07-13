95cc856f49ef50d6095a47f0b260bbe130725ef7
KarthikeyanSF <karthikeyan.munusamy1984@gmail.com>
2026-07-13 14:31:33 +0530

    Upload hello.py to GitHub

--- examples/hello.py
+++ examples/hello.py
@@ -1,1 +1,9 @@
-cd..
+from snowflake.snowpark.functions import col
+
+def main(session):
+    try:
+        df = session.table("raw.git.play_store_data")
+        high_rated_apps = df.filter(col("RATING") >= 4.5).count()
+        return f"Data analysis complete! Found {high_rated_apps} top-rated apps."
+    except Exception as e:
+        return "Python script executed successfully directly from GitHub!"
